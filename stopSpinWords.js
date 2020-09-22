6c0e750763e3a2334226327507cb1d3aea76c447
lucaastorres <lucaas.torres@live.com>
2020-09-22 17:56:06 -0300

    stopSpinWords

--- stopSpinWords.js
+++ stopSpinWords.js
@@ -1,3 +1,5 @@
 function spinWords(str){
     return str.split(' ').map(a => a.replace(a, b => b.length > 4 ? b.split('').reverse().join(''): b)).join(' ')
-  }
+}
+
+console.log(spinWords("Just kidding there is still one more"))
