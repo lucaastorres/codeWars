db2d8be9e3ff5b2dff3d29726a8a471abd549295
lucaastorres <lucaas.torres@live.com>
2020-09-22 17:54:28 -0300

    pigIt

--- simplePigLatin.js
+++ simplePigLatin.js
@@ -1,7 +1,8 @@
 function pigIt(str){
     const end = str.charAt(str.length-1)
     str = str.split(' ').map(a => a.slice(1) + (a.slice(0, 1) + "ay")).join(' ')
-    return end === "!" || end === "?"? str.slice(0, str.length-3) + end: str
-        
-    
+    return end === "!" || end === "?"? str.slice(0, str.length-3) + end: str        
+
 }
+
+console.log(pigIt('This is my string'))
